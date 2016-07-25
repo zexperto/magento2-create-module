7084699a7cb070c1271924ebc5810796ad8ae9b0
zexperto <zexperto@hotmail.com>
2016-07-25 10:02:48 +0300

    Fix Bugs

--- ITM/File/Controller/Adminhtml/Box/NewAction.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ITM\File\Controller\Adminhtml\Box;
-
-class NewAction extends \ITM\File\Controller\Adminhtml\Box {
-	public function execute() {
-		$this->_forward('edit');
-	}
-}
