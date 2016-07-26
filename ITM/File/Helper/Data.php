9ddff416d7c7865b1b4f3806826fecfb7d94a705
zexperto <zexperto@hotmail.com>
2016-07-26 17:50:37 +0300

    Add Some Comments

--- ITM/File/Helper/Data.php
+++
@@ -1,9 +0,0 @@
-<?php
-				
-namespace ITM\File\Helper;
-		
-class Data extends \Magento\Framework\App\Helper\AbstractHelper {
-	public function __construct(\Magento\Framework\App\Helper\Context $context) {
-		parent::__construct ( $context );
-	}
-}
--- ITM/File/Model/Resource/Box.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-namespace ITM\File\Model\Resource;
-	
-class Box extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb {
-	protected function _construct() {
-		$this->_init('itm_file_box', 'id');
-	}
-}
